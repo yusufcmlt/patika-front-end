c2912adfefc6195025530302b7cea825f79868d8
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-07-10 16:09:58 +0300

    react odev-1 solution

---
+++ react/odev-1/src/app.js
@@ -0,0 +1,3 @@
+import getData from "./lib/service.js";
+
+getData(1).then((data) => console.log(data));
--- react/odev-1/src/lib/service.js
+++ react/odev-1/src/lib/service.js
@@ -1,1 +1,11 @@
-async function getData() {}
+import axios from "axios";
+
+export default async function getData(userID) {
+  userID = Number(userID);
+  const url = "https://jsonplaceholder.typicode.com";
+
+  const userData = await axios.get(`${url}/users/${userID}`);
+  const userPosts = await axios.get(`${url}/posts?userId=${userID}`);
+
+  return { user: userData.data, posts: userPosts.data };
+}
