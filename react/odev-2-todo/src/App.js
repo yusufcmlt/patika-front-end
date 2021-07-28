e2ede2cc6cdaeb0f1a08d4b9599565f128f1800f
yusufcmlt <58252790+yusufcmlt@users.noreply.github.com>
2021-07-28 09:10:33 +0300

    app layout

--- react/odev-2-todo/src/App.js
+++ react/odev-2-todo/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <header>
+        <h1 className="todo__heading">todos</h1>
+      </header>
+    </>
+  );
 }
 
 export default App;
